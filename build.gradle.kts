7daea6ac390093f1c614118289eb21ee7b48ea23
MMT <uzyd00rm4n@gmail.com>
2026-07-04 00:00:08 +0200

    Refactor build.gradle.kts by removing plugins and dependencies
    Removed plugin management section and dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
+// build.gradle.kts racine
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.8.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    
-    // Glide (image loading)
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-    
-    // ExoPlayer (video player)
-    implementation("androidx.media3:media3-exoplayer:1.2.0")
-    implementation("androidx.media3:media3-ui:1.2.0")
-    
-    // YouTube IFrame API
-    implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:11.0.1")
-    
-    // RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    
-    // Testing
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
